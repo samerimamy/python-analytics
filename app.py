12592e3b2939e78d979e915f190e793f29b364d9
SamerAlimamy <salimamy@pmu.edu.sa>
2025-10-04 12:16:11 +0300

    message

--- app.py
+++ app.py
@@ -9,28 +9,28 @@ def root():
     return {"message": "Python Analytics Server is running"}
 
 @app.get("/mis")
-def aaa():
+def mis_analytics():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     csv_path = os.path.join(base_dir, "students_mis.csv")
 
     df = pd.read_csv(csv_path)
 
     result = {
-        "BySemester": df["Semester"].value_counts().to_dict(),
         "Total": int(len(df)),
+        "BySemester": df["Semester"].value_counts().to_dict(),
         "ByCourse": df["Course"].value_counts().to_dict(),
         "EmploymentRate": float(df["EmployedWithin6Months"].mean()),
         "AverageGPA": float(df["GPA"].mean())
     }
     return result
-#@app.get("/analyze_csv")
-# # def analyze_csv_analytics():
-    # # base_dir = os.path.dirname(os.path.abspath(__file__))
-    # # csv_path = os.path.join(base_dir, "students_mis.csv")
+@app.get("/analyze_csv")
+def analyze_csv_analytics():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(base_dir, "students_mis.csv")
 
-    # # df = pd.read_csv(csv_path)
+    df = pd.read_csv(csv_path)
 
-    # # result = {
-        # # print(df.to_dict(orient="records"))
-    # # }
-    # # return result
+    result = {
+        print(df.to_dict(orient="records"))
+    }
+    return result
