3cfd94e1c50d3ecddd9b7598f3780d36af077357
SamerAlimamy <salimamy@pmu.edu.sa>
2025-10-04 11:57:41 +0300

    message

--- app - Copy MIS.py
+++ app - Copy MIS.py
@@ -16,10 +16,17 @@ def mis_analytics():
     df = pd.read_csv(csv_path)
 
     result = {
-        "Total": int(len(df)),
-        "BySemester": df["Semester"].value_counts().to_dict(),
-        "ByCourse": df["Course"].value_counts().to_dict(),
-        "EmploymentRate": float(df["EmployedWithin6Months"].mean()),
-        "AverageGPA": float(df["GPA"].mean())
+        print(df.to_dict(orient="records"))
+    }
+    return result
+@app.get("/analyze_csv")
+def mis_analytics():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(base_dir, "test.csv")
+
+    df = pd.read_csv(csv_path)
+
+    result = {
+        print(df.to_dict(orient="records"))
     }
     return result
