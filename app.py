87b3cbdefea441108a7ecb4eaa9818cac4eee309
SamerAlimamy <salimamy@pmu.edu.sa>
2025-10-04 12:04:37 +0300

    message

--- app.py
+++ app.py
@@ -23,14 +23,14 @@ def mis_analytics():
         "AverageGPA": float(df["GPA"].mean())
     }
     return result
-@app.get("/analyze_csv")
-def analyze_csv_analytics():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    csv_path = os.path.join(base_dir, "students_mis.csv")
+# @app.get("/analyze_csv")
+# def analyze_csv_analytics():
+    # base_dir = os.path.dirname(os.path.abspath(__file__))
+    # csv_path = os.path.join(base_dir, "students_mis.csv")
 
-    df = pd.read_csv(csv_path)
+    # df = pd.read_csv(csv_path)
 
-    result = {
-        print(df.to_dict(orient="records"))
-    }
-    return result
+    # result = {
+        # print(df.to_dict(orient="records"))
+    # }
+    # return result
